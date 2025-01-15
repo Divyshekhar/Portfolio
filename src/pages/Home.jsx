f2bcdb79b47a594eb3210b7989ea412bcf4252b0
Divyshekhar Sinha <divyshekhar2204@gmail.com>
2025-01-15 15:40:57 +0530

    adding 1 project and correcting the colors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,6 +10,9 @@ import Express_icon from '../assets/expressjs_icon.png';
 import Postman_icon from '../assets/postman_icon.svg';
 import Mongo_icon from '../assets/Mongo_icon.png';
 import Postgres_icon from '../assets/PostGres_icon.png';
+import Memories from '../assets/Memories.png';
+import Button from '@mui/material/Button';
+import Redux_icon from '../assets/redux_icon.png';
 function Home({ darkTheme }) {
     return (
         <Box
@@ -110,9 +113,72 @@ function Home({ darkTheme }) {
                     </Typography>
                 </Grid>
                 <Grid item xs={12} sm={12} sx={{ marginTop: "50px" }}>
-                    <Typography sx={{ fontSize: '25px', fontWeight: 'bold', display: 'flex', justifyContent: 'center' }}>
+                    <Typography sx={{ fontSize: '30px', fontWeight: 'bold', display: 'flex', justifyContent: 'center' }}>
                         Projects
                     </Typography>
+                </Grid>
+                <Grid item xs={12} sm={6} sx={{ marginTop: '50px' }}>
+                    <Typography sx={{ fontSize: '25px', fontWeight: '20px', display: 'flex', justifyContent: 'left', color:'rgb(233, 177, 196)'}}>
+                        Memories<br /> <br />
+                    </Typography>
+                    <Typography sx={{ fontSize: '17px', display: 'flex', justifyContent: 'center' }}>
+                    Memories is a fullstack web application that allows users to upload photos and descriptions to create a record of their moments. Users can like posts, edit them, or delete them as needed. The app provides a simple interface for managing and viewing posts, making it easy to keep track of personal memories and interactions.
+                    <br />
+                    </Typography>
+                    <Typography sx={{color: darkTheme ? 'rgb(254,246,179)' : 'rgb(21,94,149)', fontWeight: 'bold'}}>Tech stack used: </Typography><br/>
+                    <img
+                            src={Mongo_icon}
+                            title="MongoDB"
+                            alt="MongoDB"
+                            style={{ height: 'auto', width: '5%', borderRadius: "20%", marginRight: '10px' }}
+
+                        />
+                    <img
+                            src={Express_icon}
+                            title="Express JS"
+                            alt="Express"
+                            style={{ height: 'auto', width: '5%', borderRadius: "20%", marginRight: '10px' }}
+
+                        />
+                    <img
+                            src={React_icon}
+                            title="React"
+                            alt="React"
+                            style={{ height: 'auto', width: '5%', borderRadius: "20%", marginRight: '8px' }}
+                        />
+                    <img
+                            src={Node_icon}
+                            title="Node JS"
+                            alt="Node"
+                            style={{ height: 'auto', width: '5%', borderRadius: "20%", marginRight: '10px' }}
+
+                        />
+                    <img
+                            src={JS_icon}
+                            title="Javascript"
+                            alt="Javascript"
+                            style={{ height: 'auto', width: '5.3%', borderRadius: "20%", marginRight: '10px' }}
+
+                        />
+                    <img
+                            src={Redux_icon}
+                            title="Redux"
+                            alt="Redux"
+                            style={{ height: 'auto', width: '5%', borderRadius: "20%", marginRight: '10px' }}
+
+                        />
+                    <br />
+                    <br />
+                    <Button variant="contained" sx={{ borderRadius: '25px', background: 'rgb(183,113,228)', color:'rgb(177, 215, 233)', marginRight: '20px'}} onClick={() => window.open('https://memories-frontend-yv72.onrender.com/', '_blank')} >See Live</Button>
+                    <Button variant="contained" sx={{ borderRadius: '25px', background: 'rgb(183,113,228)', color:'rgb(177, 215, 233)' }} onClick={() => window.open('https://github.com/Divyshekhar/Memories_Frontend', '_blank')} >Source Code</Button>
+                </Grid>
+                <Grid item xs={12} sm={6} sx={{ marginTop: '50px' }}>
+                    <img 
+                        src={Memories}
+                        title="Memories"
+                        alt="Memories"
+                        style={{ width: '80%', borderRadius: '20px', marginLeft: '50px'}}
+                    />
                 </Grid>
 
 
