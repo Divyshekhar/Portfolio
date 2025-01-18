b34fb4e3acd14c0fd431eddea6d17b45d892f86b
Divyshekhar Sinha <divyshekhar2204@gmail.com>
2025-01-19 01:00:40 +0530

    fixing responsiveness

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -56,47 +56,6 @@ function NavBar({ darkTheme, setDarkTheme }) {
             {/* This is the desktop name */}
             Divyshekhar Sinha
           </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{ display: { xs: 'block', md: 'none' } }}
-            >
-              {/* This is the small display menu items */}
-              {pages.map((page) => (
-                <MenuItem key={page.name} onClick={handleCloseNavMenu}>
-                  <Typography sx={{ textAlign: 'center' }}>
-                    <Link to={page.path} style={{ textDecoration: 'none', color: 'inherit' }}>
-                      {page.name}
-                    </Link>
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-            
-          </Box>
           <Typography
             variant="h5"
             noWrap
@@ -115,7 +74,7 @@ function NavBar({ darkTheme, setDarkTheme }) {
             }}
           >
             {/* This is the small display name */}
-            Divyshekhar Sinha Small
+            Divyshekhar Sinha
           </Typography>
           
           <Box sx={{ flexGrow: 1,  display:'flex', justifyContent: 'end'}}>
