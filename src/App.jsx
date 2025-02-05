f80a523ed8f49ad4ad429b3769b3a1fc70191402
Divyshekhar Sinha <divyshekhar2204@gmail.com>
2025-02-05 23:05:10 +0530

    changing routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Redirect } from "react-router-dom";
 import Footer from './components/Footer';
 function App() {
     const [darkTheme, setDarkTheme] = useState(false);
@@ -14,7 +14,8 @@ function App() {
             <NavBar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
             <Routes>
                 <Route path='/' element={<Home darkTheme={darkTheme} />} />
-                <Route path="*" element={<NotFound darkTheme={darkTheme} />} />
+                <Route path="/404" element={<NotFound darkTheme={darkTheme} />} />
+                {/* <Redirect from='*' to='/404' /> */}
             </Routes>
             <Footer />
         </Router>
